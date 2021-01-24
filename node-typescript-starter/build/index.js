c586d74aa9b590600b7ce4483034730143245f13
Eric Cabrel TIOGO <eric@ovrsea.com>
2021-01-24 21:29:32 +0100

    Node Typescript tutorial

---
+++ node-typescript-starter/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var addition = function (a, b) {
+    return a + b;
+};
+var number1 = 5;
+var number2 = 10;
+var result = addition(number1, number2);
+console.log('The result is %d', result);
---
+++ node-typescript-starter/src/index.ts
@@ -0,0 +1,9 @@
+const addition = (a: number, b: number): number => {
+    return a + b;
+};
+
+const number1: number = 5;
+const number2: number = 10;
+const result: number = addition(number1, number2);
+
+console.log('The result is %d', result);
