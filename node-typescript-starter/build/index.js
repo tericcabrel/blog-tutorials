c0bfefc794009c8dd5cd907f7a5944996ac45d0e
Eric Cabrel TIOGO <eric@ovrsea.com>
2021-01-26 23:52:15 +0100

    Finalize node typescript starter tutorial

--- node-typescript-starter/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var addition = function (a, b) {
-    return a + b;
-};
-var number1 = 5;
-var number2 = 10;
-var result = addition(number1, number2);
-console.log('The result is %d', result);
--- node-typescript-starter/src/index.ts
+++ node-typescript-starter/src/index.ts
@@ -1,9 +1,9 @@
-const addition = (a: number, b: number): number => {
+const addition = (a: number, b: number) => {
     return a + b;
 };
 
-const number1: number = 5;
-const number2: number = 10;
-const result: number = addition(number1, number2);
+const number1 = 5;
+const number2 = 10;
+const result = addition(number1, number2);
 
-console.log('The result is %d', result);
+console.log("The result is %d", result);
