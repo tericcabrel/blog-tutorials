5594705e2f7820fd34c9500c7dcf21f1e16b5178
Eric Cabrel TIOGO <eric@ovrsea.com>
2021-02-15 23:45:30 +0100

    Upload file to Google Drive API

---
+++ node-gdrive-api/src/googleDriveService.ts
@@ -0,0 +1,78 @@
+import fs from 'fs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { google } = require('googleapis');
+
+/**
+ * Browse the link below to see the complete object returned for folder/file creation and search
+ *
+ * @link https://developers.google.com/drive/api/v3/reference/files#resource
+ */
+type PartialDriveFile = {
+  id: string;
+  name: string;
+};
+
+type SearchResultResponse = {
+  kind: 'drive#fileList';
+  nextPageToken: string;
+  incompleteSearch: boolean;
+  files: PartialDriveFile[];
+};
+
+export class GoogleDriveService {
+  private driveClient;
+
+  public constructor(clientId: string, clientSecret: string, redirectUri: string, refreshToken: string) {
+    this.driveClient = this.createDriveClient(clientId, clientSecret, redirectUri, refreshToken);
+  }
+
+  createDriveClient(clientId: string, clientSecret: string, redirectUri: string, refreshToken: string) {
+    const client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
+    client.setCredentials({ refresh_token: refreshToken });
+
+    return google.drive({
+      version: 'v3',
+      auth: client,
+    });
+  }
+
+  createFolder(folderName: string): Promise<PartialDriveFile> {
+    return this.driveClient.files.create({
+      resource: {
+        name: folderName,
+        mimeType: 'application/vnd.google-apps.folder',
+      },
+      fields: 'id, name',
+    });
+  }
+
+  searchFolder(folderName: string): Promise<PartialDriveFile | null> {
+    return new Promise((resolve, reject) => {
+      this.driveClient.files.list(
+        {
+          q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}'`,
+          fields: 'files(id, name)',
+        },
+        (err, res: { data: SearchResultResponse }) => {
+          if (err) return reject(err);
+
+          return resolve(res.data.files ? res.data.files[0] : null);
+        },
+      );
+    });
+  }
+
+  saveFile(fileName: string, filePath: string, fileMimeType: string, folderId?: string) {
+    return this.driveClient.files.create({
+      requestBody: {
+        name: fileName,
+        mimeType: fileMimeType,
+        parents: folderId ? [folderId] : [],
+      },
+      media: {
+        mimeType: fileMimeType,
+        body: fs.createReadStream(filePath),
+      },
+    });
+  }
+}
--- node-gdrive-api/src/index.ts
+++ node-gdrive-api/src/index.ts
@@ -1,9 +1,38 @@
-const addition = (a: number, b: number) => {
-  return a + b;
-};
+import dotenv from 'dotenv';
+import * as path from 'path';
+import * as fs from 'fs';
 
-const number1 = 5;
-const number2 = 10;
-const result = addition(number1, number2);
+import { GoogleDriveService } from './googleDriveService';
 
-console.log('The result is %d', result);
+dotenv.config();
+
+const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || '';
+const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
+const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
+const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
+
+(async () => {
+  const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+
+  const finalPath = path.resolve(__dirname, '../public/spacex-uj3hvdfQujI-unsplash.jpg');
+  const folderName = 'Picture';
+
+  if (!fs.existsSync(finalPath)) {
+    throw new Error('File not found!');
+  }
+
+  let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
+    console.error(error);
+    return null;
+  });
+
+  if (!folder) {
+    folder = await googleDriveService.createFolder(folderName);
+  }
+
+  await googleDriveService.saveFile('SpaceX', finalPath, 'image/jpg', folder.id).catch((error) => {
+    console.error(error);
+  });
+
+  console.info('File uploaded successfully!');
+})();
