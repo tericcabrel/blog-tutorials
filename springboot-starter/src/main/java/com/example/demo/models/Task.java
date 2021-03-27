6f7f905c031a6c255f4499481e91e2eeb0874731
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-03-27 13:32:16 +0100

    chore: create model for task

---
+++ springboot-starter/src/main/java/com/example/demo/models/Task.java
@@ -0,0 +1,47 @@
+package com.example.demo.models;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Table(name = "tasks", indexes = {
+    @Index(name = "idx_title", columnList = "title")
+})
+@Entity()
+public class Task {
+  @Id()
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(insertable = false, updatable = false)
+  private int id;
+
+  @Column(length = 100, unique = true)
+  private String title;
+
+  @Lob()
+  private String description;
+
+  @Column(length = 20)
+  private TaskStatusEnum status = TaskStatusEnum.PENDING;
+
+  @Column(name = "max_duration")
+  private int maxDuration;
+
+  @Column(name = "is_public", columnDefinition = "boolean default false")
+  private boolean isPublic;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  private Date createdAt;
+
+  @UpdateTimestamp
+  @Column(nullable = false)
+  private Date updatedAt;
+}
---
+++ springboot-starter/src/main/java/com/example/demo/models/TaskStatusEnum.java
@@ -0,0 +1,7 @@
+package com.example.demo.models;
+
+public enum TaskStatusEnum {
+  PENDING,
+  IN_PROGRESS,
+  COMPLETED
+}
