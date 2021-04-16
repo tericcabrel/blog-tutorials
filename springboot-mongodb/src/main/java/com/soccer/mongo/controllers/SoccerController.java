6c9df21fe4a9cc271b2d445909e054d2dd03763b
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-04-16 22:12:11 +0200

    feat: create endpoint to update, delete a team and also create many players

--- springboot-mongodb/src/main/java/com/soccer/mongo/controllers/SoccerController.java
+++ springboot-mongodb/src/main/java/com/soccer/mongo/controllers/SoccerController.java
@@ -6,10 +6,16 @@ import com.soccer.mongo.models.Player;
 import com.soccer.mongo.models.Team;
 import com.soccer.mongo.repositories.PlayerRepository;
 import com.soccer.mongo.repositories.TeamRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,5 +41,42 @@ public class SoccerController {
     Player playerCreated = playerRepository.save(createPlayerDto.toPlayer());
 
     return new ResponseEntity<>(playerCreated, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/teams/{id}")
+  public ResponseEntity<Team> updateTeam(@RequestParam String id, @RequestBody CreateTeamDto createTeamDto) {
+    Optional<Team> optionalTeam = teamRepository.findById(id);
+
+    if (optionalTeam.isEmpty()) {
+      return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    Team teamToUpdate = optionalTeam.get()
+          .setAddress(createTeamDto.getAddress())
+          .setName(createTeamDto.getName())
+          .setAcronym(createTeamDto.getAcronym());
+
+    Team teamUpdated =  teamRepository.save(teamToUpdate);
+
+    return new ResponseEntity<>(teamUpdated, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/teams/{id}")
+  public ResponseEntity<Void> deleteTeam(@RequestParam String id, @RequestBody CreateTeamDto createTeamDto) {
+    teamRepository.deleteById(id);
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @PostMapping("/players/bulk")
+  public ResponseEntity<List<Player>> createPlayers(@RequestBody List<CreatePlayerDto> createPlayerDtoList) {
+    List<Player> players = createPlayerDtoList
+        .stream()
+        .map(CreatePlayerDto::toPlayer)
+        .collect(Collectors.toList());
+
+    List<Player> playersCreated = playerRepository.saveAll(players);
+
+    return new ResponseEntity<>(playersCreated, HttpStatus.CREATED);
   }
 }
