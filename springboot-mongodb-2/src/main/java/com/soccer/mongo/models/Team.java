dae31f8250f933e52da89595514a3ef70a48acb0
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-05-30 23:23:32 +0200

    feat: retrieve custom query

--- springboot-mongodb-2/src/main/java/com/soccer/mongo/models/Team.java
+++ springboot-mongodb-2/src/main/java/com/soccer/mongo/models/Team.java
@@ -1,6 +1,5 @@
 package com.soccer.mongo.models;
 
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
--- springboot-mongodb-2/src/main/java/com/soccer/mongo/repositories/PlayerRepository.java
+++ springboot-mongodb-2/src/main/java/com/soccer/mongo/repositories/PlayerRepository.java
@@ -1,10 +1,14 @@
 package com.soccer.mongo.repositories;
 
 import com.soccer.mongo.models.Player;
+import com.soccer.mongo.models.PlayerPosition;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlayerRepository extends MongoRepository<Player, String> {
+  List<Player> findByPositionAndIsAvailable(PlayerPosition playerPosition, boolean isAvailable);
 
+  List<Player> findDistinctNameByPositionIn(List<PlayerPosition> playerPositions);
 }
