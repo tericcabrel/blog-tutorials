1dc74f79928e14fd2b3f78a1ae32958fc380627c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-05-30 23:57:34 +0200

    feat: retrieve data with custom queries 1

--- springboot-mongodb-2/src/main/java/com/soccer/mongo/repositories/PlayerRepository.java
+++ springboot-mongodb-2/src/main/java/com/soccer/mongo/repositories/PlayerRepository.java
@@ -2,6 +2,7 @@ package com.soccer.mongo.repositories;
 
 import com.soccer.mongo.models.Player;
 import com.soccer.mongo.models.PlayerPosition;
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,8 @@ public interface PlayerRepository extends MongoRepository<Player, String> {
   List<Player> findByPositionAndIsAvailable(PlayerPosition playerPosition, boolean isAvailable);
 
   List<Player> findDistinctNameByPositionIn(List<PlayerPosition> playerPositions);
+
+  List<Player> findByBirthDateIsBetween(Date fromDate, Date toDate);
+
+  Player findFirstByOrderByBirthDateDesc();
 }
