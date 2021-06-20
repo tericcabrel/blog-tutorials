9dcf69391c8cd0f3389fd6e8d63b71343d1ae4fa
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-06-20 23:40:16 +0200

    feat: create project for spring boot validation

---
+++ springboot-validation/src/test/java/com/tericcabrel/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
