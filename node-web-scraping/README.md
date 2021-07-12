362a0e418b648c27701f7f1ceb0fc2fe4072c91b
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-07-12 14:28:50 +0200

    feat: init node web scraping project

---
+++ node-web-scraping/README.md
@@ -0,0 +1,25 @@
+# Node Typescript Starter
+
+This project contains a minimal starter for Node.js project with Typescript, ESLint and Prettier already configured
+
+## Prerequisites
+- Node.js 10+
+- Yarn or NPM
+
+## Installation
+- Install dependencies
+```bash
+yarn install
+```
+
+- Create application configuration
+```bash
+cp .env.example .env
+nano .env
+```
+
+- Start Application
+```bash
+yarn start
+```
+The application will be launched by [Nodemon](https://nodemon.com). It will restart automatically on file change
---
+++ node-web-scraping/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectToDatabase } from './databaseConnection';
+
+dotenv.config();
+
+const HOST = process.env.HOST || 'http://localhost';
+const PORT = parseInt(process.env.PORT || '4500');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World!' });
+});
+
+app.listen(PORT, async () => {
+  await connectToDatabase();
+
+  console.log(`Application started on URL ${HOST}:${PORT} 🎉`);
+});
