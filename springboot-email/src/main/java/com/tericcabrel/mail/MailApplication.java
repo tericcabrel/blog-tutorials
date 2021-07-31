b12eac9d8952eed1ca75de0dc883663de57a72e0
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-07-31 16:36:54 +0200

    feat: send email with spring boot

--- springboot-email/src/main/java/com/tericcabrel/mail/MailApplication.java
+++ springboot-email/src/main/java/com/tericcabrel/mail/MailApplication.java
@@ -2,8 +2,9 @@ package com.tericcabrel.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ThymeleafAutoConfiguration.class })
 public class MailApplication {
 
 	public static void main(String[] args) {
