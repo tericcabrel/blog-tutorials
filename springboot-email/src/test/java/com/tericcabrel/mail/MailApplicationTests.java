f4da2a18586c275f22e58f09446b9ea6614834cf
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-07-31 13:09:42 +0200

    feat: setup spring boot email project

---
+++ springboot-email/src/test/java/com/tericcabrel/mail/MailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
