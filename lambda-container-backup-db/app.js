58c34a04b42d4201cdfbfdf8ebe3ead3ef3ce459
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-08-23 22:45:59 +0200

    feat: create repository for lambda container backup

---
+++ lambda-container-backup-db/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const childProcess = require("child_process");
+const path = require("path");
+
+const backupDatabase = () => {
+  const scriptFilePath = path.resolve(__dirname, "./backup.sh");
+
+  return new Promise((resolve, reject) => {
+    childProcess.execFile(scriptFilePath, (error) => {
+      if (error) {
+        console.error(error);
+        resolve(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
+
+module.exports.handler = async (event) => {
+  const isBackupSuccessful = await backupDatabase();
+
+  if (isBackupSuccessful) {
+    return {
+      status: "success",
+      message: "Database backup completed successfully!"
+    };
+  }
+
+  return  {
+    status: "failed",
+    message: "Failed to backup the database! Check out the logs for more details"
+  };
+};
