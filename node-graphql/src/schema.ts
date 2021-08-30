dbd2b6652f6add304bf4922e78b4b17e160fb942
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-08-30 22:08:37 +0200

    feat: create graphql schema and generate types from the schema

---
+++ node-graphql/src/schema.ts
@@ -0,0 +1,28 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+
+  input CreateUserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+
+  type Mutation {
+    registerUser(input: CreateUserInput): User!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+`;
+
+export default typeDefs;
