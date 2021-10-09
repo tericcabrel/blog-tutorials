338def00105120e378b07d33ea24ce3fb586d6f0
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-10-09 17:59:35 +0200

    feat: update the test file + .gitignore

--- starter/src/test/java/com/tericcabrel/AppTest.java
+++ starter/src/test/java/com/tericcabrel/AppTest.java
@@ -1,38 +1,16 @@
 package com.tericcabrel;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+public class AppTest {
+    @Test
     public void testApp()
     {
+        System.out.println("Hello world from test!");
         assertTrue( true );
     }
 }
