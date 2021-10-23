ec64887b74ca41122b2482b8ff2b323a09a81750
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-10-23 23:31:58 +0200

    feat: create project for spring boot flyway

---
+++ springboot-flyway/src/main/java/com/tericcabrel/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
