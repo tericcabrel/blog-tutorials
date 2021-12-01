a33c3417e183263297ba85439aea04b02fd00a44
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-12-01 22:57:01 +0100

    feat: test the lambda locally

--- node-lambda-timeviewer/lib/node-lambda-timeviewer-stack.ts
+++ node-lambda-timeviewer/lib/node-lambda-timeviewer-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import * as path from "path";
 
 export class NodeLambdaTimeviewerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     new lambda.NodejsFunction(this, 'TimeViewer', {
-      entry: '../src/time-viewer.ts', // accepts .js, .jsx, .ts and .tsx files
+      entry: path.resolve(__dirname, '../src/time-viewer.ts'), // accepts .js, .jsx, .ts and .tsx files
       functionName: 'timeViewer',
       handler: 'handler',
       memorySize: 512,
