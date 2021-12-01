40cb4023494271e342f50b3703d1bbf64290c4a5
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-12-01 21:59:17 +0100

    feat: create lambda function with the CDK

--- node-lambda-timeviewer/lib/node-lambda-timeviewer-stack.ts
+++ node-lambda-timeviewer/lib/node-lambda-timeviewer-stack.ts
@@ -1,18 +1,16 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 
 export class NodeLambdaTimeviewerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'NodeLambdaTimeviewerQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    new lambda.NodejsFunction(this, 'TimeViewer', {
+      entry: '../src/time-viewer.ts', // accepts .js, .jsx, .ts and .tsx files
+      functionName: 'timeViewer',
+      handler: 'handler',
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(10)
     });
-
-    const topic = new sns.Topic(this, 'NodeLambdaTimeviewerTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
