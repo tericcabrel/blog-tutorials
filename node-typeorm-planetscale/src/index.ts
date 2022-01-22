8e4b8ee65a598c5e0088e248389dd4c6fcf242f6
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-22 18:03:19 +0100

    feat: create task model and migration

--- node-typeorm-planetscale/src/index.ts
+++ node-typeorm-planetscale/src/index.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {Task} from "./entity/Task";
 
 createConnection().then(async connection => {
 
     console.log("Inserting a new user into the database...");
-    const user = new User();
+    const user = new Task();
     user.firstName = "Timber";
     user.lastName = "Saw";
     user.age = 25;
@@ -13,7 +13,7 @@ createConnection().then(async connection => {
     console.log("Saved a new user with id: " + user.id);
 
     console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
+    const users = await connection.manager.find(Task);
     console.log("Loaded users: ", users);
 
     console.log("Here you can setup and run express/koa/any other framework.");
