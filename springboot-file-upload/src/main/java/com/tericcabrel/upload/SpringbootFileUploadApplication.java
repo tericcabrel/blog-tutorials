55ea95c97d2839e81bdf087a00d5856c665722c8
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-02-05 14:30:33 +0100

    feat: upload the file to the server

---
+++ springboot-file-upload/src/main/java/com/tericcabrel/upload/SpringbootFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFileUploadApplication.class, args);
+	}
+
+}
---
+++ springboot-file-upload/src/test/java/com/tericcabrel/upload/SpringbootFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
