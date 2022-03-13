a5eefbdb5df97622daa4e4ac60f637265b1a06f5
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-03-13 18:34:17 +0100

    create the database schema from the entities

---
+++ springboot-many-to-many/src/main/java/com/tericcabrel/movie/entities/Movie.java
@@ -0,0 +1,61 @@
+package com.tericcabrel.movie.entities;
+
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Table(name = "movies")
+@Entity
+public class Movie {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(nullable = false)
+  private Integer id;
+
+  @Column(nullable = false, length = 150)
+  private String name;
+
+  @Lob
+  private String description;
+
+  @Column(nullable = false)
+  private Integer releaseDate;
+
+  @ManyToMany(mappedBy = "movies")
+  private Set<User> users;
+
+  public Movie() {
+  }
+
+  public Movie(String name, String description, Integer releaseDate) {
+    this.name = name;
+    this.description = description;
+    this.releaseDate = releaseDate;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Integer getReleaseDate() {
+    return releaseDate;
+  }
+
+  public Set<User> getUsers() {
+    return users;
+  }
+}
