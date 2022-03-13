04399425a76b766c99a4edc73302a477523fc325
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-03-13 15:15:23 +0100

    create project for many to many with springboot

---
+++ springboot-many-to-many/src/main/java/com/tericcabrel/movie/MovieApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
