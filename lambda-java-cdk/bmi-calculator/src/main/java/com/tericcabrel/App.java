23e8533a6530230f94b5f3ee8c2f112f88a6f597
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-03-26 18:29:15 +0100

    feat: write the lambda function stack and logic

--- lambda-java-cdk/bmi-calculator/src/main/java/com/tericcabrel/App.java
+++ lambda-java-cdk/bmi-calculator/src/main/java/com/tericcabrel/App.java
@@ -1,13 +1,26 @@
 package com.tericcabrel;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Content-Type", "application/json");
+        responseHeaders.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(responseHeaders);
+
+        String output = String.format("{ \"message\": \"hello world\" }");
+
+        return response
+            .withStatusCode(200)
+            .withBody(output);
     }
 }
--- lambda-java-cdk/infra/src/main/java/com/myorg/InfraStack.java
+++ lambda-java-cdk/infra/src/main/java/com/myorg/InfraStack.java
@@ -1,12 +1,22 @@
 package com.myorg;
 
+import static java.util.Collections.singletonList;
+import static software.amazon.awscdk.BundlingOutput.ARCHIVED;
+
+import java.util.Arrays;
+import java.util.List;
 import software.constructs.Construct;
+import software.amazon.awscdk.BundlingOptions;
+import software.amazon.awscdk.DockerVolume;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
 
 public class InfraStack extends Stack {
     public InfraStack(final Construct parent, final String id) {
@@ -16,14 +26,40 @@ public class InfraStack extends Stack {
     public InfraStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "InfraQueue")
-                .visibilityTimeout(Duration.seconds(300))
-                .build();
+        List<String> lambdaFunctionPackagingInstructions = Arrays.asList(
+            "/bin/sh",
+            "-c",
+            "cd bmi-calculator " +
+                "&& mvn clean install " +
+                "&& cp /asset-input/bmi-calculator/target/bmicalculator.jar /asset-output/"
+        );
 
-        final Topic topic = Topic.Builder.create(this, "InfraTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+            .command(lambdaFunctionPackagingInstructions)
+            .image(Runtime.JAVA_11.getBundlingImage())
+            .volumes(singletonList(
+                // Mount local .m2 repo to avoid download all the dependencies again inside the container
+                DockerVolume.builder()
+                    .hostPath(System.getProperty("user.home") + "/.m2/")
+                    .containerPath("/root/.m2/")
+                    .build()
+            ))
+            .user("root")
+            .outputType(ARCHIVED);
 
-        topic.addSubscription(new SqsSubscription(queue));
+        Function bmiCalculatorFunction = new Function(this, "bmi-calculator", FunctionProps.builder()
+            .functionName("bmi-calculator")
+            .runtime(Runtime.JAVA_11)
+            .code(Code.fromAsset("../", AssetOptions.builder().bundling(
+                builderOptions.command(
+                    lambdaFunctionPackagingInstructions
+                ).build()
+            ).build()
+            ))
+            .handler("com.tericcabrel.App")
+            .memorySize(1024)
+            .timeout(Duration.seconds(10))
+            .logRetention(RetentionDays.ONE_WEEK)
+            .build());
     }
 }
