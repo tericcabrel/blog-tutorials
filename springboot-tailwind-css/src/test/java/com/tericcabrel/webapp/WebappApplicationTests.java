d4e0f001ea85361a825acd0d96956a1701d6c5cd
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-05-14 12:08:58 +0200

    feat: create project for spring boot and tailwind css

---
+++ springboot-tailwind-css/src/test/java/com/tericcabrel/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
