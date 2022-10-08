66db5a31f49b6d018d6dcb95dfbeb6fcff15400c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-10-08 15:35:41 +0200

    feat: create project for spring boot rest api

---
+++ springboot-rest-api/src/main/java/com/tericcabrel/taskman/TaskmanApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.taskman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanApplication.class, args);
+	}
+
+}
---
+++ springboot-rest-api/src/test/java/com/tericcabrel/taskman/TaskmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.taskman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
