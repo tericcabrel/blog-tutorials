623b5ddd863defcac7763d0dd86f85dd93cb524c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-11-01 15:38:27 +0100

    feat: initialize project for node graphql unions

---
+++ node-graphql-unions/src/datasource.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    email: 'tlannister@got.com',
+    firstName: 'Tywin',
+    id: '1',
+    lastName: 'Lannister',
+    password: 'my-very-secured-password',
+  },
+  {
+    email: 'lmormont@got.com',
+    firstName: 'Lyanna',
+    id: '2',
+    lastName: 'Mormont',
+    password: 'my-very-secured-password',
+  },
+];
