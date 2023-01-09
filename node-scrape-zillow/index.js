c455966bbb850343649acc6a3162c1987dc38c9c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2023-01-09 23:56:10 +0100

    feat: update script to scrape zillow

--- node-scrape-zillow/index.js
+++ node-scrape-zillow/index.js
@@ -1,17 +1,3 @@
-/* const unirest = require('unirest');
-
-async function zillow() {
-    const target_url = "https://www.zillow.com/homes/for_sale/";
-
-    const zillow_data = await unirest.get(target_url);
-
-    return zillow_data.body;
-}
-
-zillow().then((data) => {
-    console.log(data);
-});*/
-
 const unirest = require('unirest');
 const cheerio = require('cheerio');
 
@@ -19,10 +5,12 @@ async function zillow() {
     const target_url="https://www.zillow.com/homes/for_sale/";
     const zillow_data = await unirest.get(target_url);
 
+    console.log(zillow_data.body);
+
     const $ = cheerio.load(zillow_data.body);
 
     if (zillow_data.statusCode === 200) {
-        $('.list-card-info').each(function() {
+        $('.list-card-info').each(function () {
             const price = $(this).find('.list-card-price').text();
             const address = $(this).find('.list-card-addr').text();
             const bedrooms = $(this).find('.list-card-details li:nth-child(1)').text();
